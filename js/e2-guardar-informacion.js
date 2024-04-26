9fde00fc3760e580c0f8c0e075a98f4b72994fc9
SoniaRomojaro <soniaromojaro@gmail.com>
2024-04-26 18:36:31 +0200

    ejemplos

---
+++ js/e2-guardar-informacion.js
@@ -0,0 +1,55 @@
+//1. DECLARAR UNA VARIABLE
+//Nombre de variable recomendado en minuscula
+let variable;
+//Nombre de variable si tiene espacio, usar camelCase, que es la primera palabra minuscula y la siguiente mayuscula sin espacios
+let miVariable;
+//Podeis usar guiones para separar los espacios pero es mas recomendable camelCase.
+let mi_variable;
+//Los nombres de las variables no pueden comenzar por un numero 2Nombre
+//Los nombres de las variables pueden tener un numero siempre y cuando no empiece por el
+let variable2;
+
+//2.INICIAR UNA VARIABLE
+//Primero declarar la variable y luego iniciarla.
+document.write("EJEMPLO 1: INICIAR UNA VARIABLE CIUDAD </br>");
+
+let ciudad;
+ciudad = "Madrid";
+document.write("La variable ciudad tiene el valor: " + ciudad);
+
+//Declarar la variable e iniciarla a la vez
+document.write("</br> EJEMPLO 2: INICIAR UNA VARIABLE EDAD </br>");
+let edad = 33;
+//cambiamos el valor de la variable edad
+edad = 34;
+document.write("La variable edad tiene el valor: " + edad);
+
+
+//TIPOS DE VARIABLES
+//NUMERO ENTERO O CON DECIMALES. LOS DECIMALES VAN CON PUNTO. Y SIEMPRE SIN COMILLAS LOS NUMEROS.
+document.write("</br> EJEMPLO 3: CREAR UNA VARIABLE DE TIPO NUMERO </br>");
+let numero = 5;
+let numeroDecimales = 5.5;
+document.write("El numero sin decimales es:" + numero);
+document.write("</br>");
+document.write("El numero con decimales es "+ numeroDecimales);
+//CADENAS. Pueden usarse comillas simples o dobles. No las mezcles, usa solo una de ellas. Cuidado con caracteres especiales, no pueden ir comillas del mismo tipo dentro del texto.
+document.write("</br> EJEMPLO 4: CREAR UNA VARIABLE DE TIPO TEXTO - STRING </br>");
+let saludo = "Hola que tal?";
+let despedida = 'Adios me voy';
+let numeroEnTexto = "33";
+document.write("La variable de texto saludo es: "+ saludo);
+document.write("</br>");
+document.write(despedida);
+document.write("</br>");
+document.write(numeroEnTexto);
+//BOOLEANOS. true o false. En minuscula y sin comillas.
+document.write("</br> EJEMPLO 5: CREAR UNA VARIABLE DE TIPO BOOLEANO </br>");
+let estaSoleado = true;
+let esDeDia = false;
+document.write("La variable booleana esta soleado es: " + estaSoleado);
+document.write("</br>");
+document.write("La variable booleana es de dia es:" + esDeDia);
+
+//EJERCICIOS PARA PRACTICAR
+document.write("</br> EJERCICIOS PARA PRACTICAR </br>");
