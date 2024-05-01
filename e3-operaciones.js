5df5b46f5b947b831d2c33f64c5b8106e7c09365
SoniaRomojaro <soniaromojaro@gmail.com>
2024-05-01 12:10:09 +0200

    operaciones

--- e3-operaciones.js
+++ e3-operaciones.js
@@ -1,45 +1,70 @@
-//SUMA
-let precioTomates = 2.5;
-let precioManzanas = 1.5; 
-let precioTotal; 
+//SUMAN DOS NUMEROS
+//Sumar el precio de tomates y manzanas
+let precioTomates = 2.8;
+let precioManzanas = 1.5;
+let precioTotal;
 
 precioTotal = precioTomates + precioManzanas;
 
-document.write("Sumar dos numeros. El precio de los tomates " + precioTomates); 
-document.write("Mas precio manzanas. El precio de las manzanas " + precioManzanas); 
-document.write("es precio" + precioTotal); 
+document.write("Sumar dos numeros: La suma del precio de los tomates " + precioTomates);
+document.write(" mas el precio de las manzanas " + precioManzanas);
+document.write(" es en total: " + precioTotal);
 document.write("<br/>");
 
-
-
-
-//RESTA
-let descuento = 1; 
+//RESTAR DOS NUMERO
+//Restarle a los tomates el descuento
+let descuento = 1;
 let precioTomatesDescuento;
 
-precioTomatesDescuento = precioTomates -descuento;
+precioTomatesDescuento = precioTomates - descuento;
+document.write("Restar dos numeros: La resta del precio de los tomates tomates " + precioTomates);
+document.write(" menos el descuento " + descuento);
+document.write(" seria: " + precioTomatesDescuento);
+document.write("<br/>");
 
+//MULTIPLICAR DOS NUMEROS
+//Calcular el precio de 5 kilos de manzanas.
+let kilosManzanas = 5;
+let precioMultiplicacion;
 
+precioMultiplicacion = kilosManzanas * precioManzanas;
+document.write("Multiplicar dos numeros: El precio total de " + kilosManzanas + " kilos de manzanas ");
+document.write(" a un precio de " + precioManzanas);
+document.write(" seria en total: " + precioMultiplicacion);
+document.write("<br/>");
 
-//MULTIPLICAR
-let kilosManzanas = 5; 
-let precioMultiplicacion = kilosManzanas * precioManzanas;
-document.write("multiplizar dos numeros: El precio de ")
+//DIVIDIR DOS NUMEROS
+//Calcular la mitad del precio de las manzanas.
+let mitad = 2;
+let mitadPrecio = precioManzanas / mitad;
 
-//DIVIDIR
-let mitad = 2; 
-let mitadPrecio  =precioManzanas / 2;
+document.write("Dividir dos numeros: El resultado de dividir el precio de las manzanas " + precioManzanas );
+document.write(" entre " + mitad);
+document.write(" seria en total: " + mitadPrecio);
+document.write("<br/>");
 
-//combinar operaciones
+//COMBINAR VARIAS OPERACIONES
+//Usar parentesis en las operaciones que querais hacer primero.
 let notaExamen1 = 5.6;
 let notaExamen2 = 8.2;
-let notaExamen3 = 6.5; 
-let totaExamenes = 3; 
+let notaExamen3 = 6.5;
+let totalExamenes = 3;
 let notaMedia;
 
-notaMedia = (notaExamen1 + notaExamen2 + notaExamen3)/totaExamenes;
-document.write("Combinar varias " + notaExamen1 + " ")
+notaMedia = (notaExamen1 + notaExamen2 + notaExamen3)/totalExamenes;
+document.write("Combinar varias operaciones: La media de los examenes " + notaExamen1 + " " + notaExamen2 + " " + notaExamen3 );
+document.write(" seria en total: " + notaMedia);
+document.write("<br/>");
 
-//operadores de incremento
-l
-
+//OPERADORES INCREMENTO Y DECREMENTO
+//++ suma uno cada vez que se usa, y el -- resta uno cada vez que se usa.
+let numeroIntentos = 5;
+document.write("El numero de intentos es " + numeroIntentos);
+document.write("<br/>");
+numeroIntentos++;
+numeroIntentos++;
+document.write("El numero de intentos incrementado es " + numeroIntentos);
+document.write("<br/>");
+numeroIntentos--;
+document.write("El numero de intentos decrementado es " + numeroIntentos);
+document.write("<br/>");
