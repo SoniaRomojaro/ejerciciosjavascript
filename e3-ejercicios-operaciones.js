bfa89f21ef4a7051aa3e94e799684b9920311f5b
SoniaRomojaro <soniaromojaro@gmail.com>
2024-05-01 14:10:39 +0200

    ejercicios operaciones

---
+++ e3-ejercicios-operaciones.js
@@ -0,0 +1,29 @@
+//AQUI HACER LOS EJERCICIOS DEL TEMA 3 DE OPERACIONES
+let precioTomates = 2.8;
+let precioManzanas = 1.5;
+let precioTotal;
+
+precioTotal = precioTomates + precioManzanas;
+
+document.write("Sumar dos numeros: La suma del precio de los tomates " + precioTomates);
+document.write(" mas el precio de las manzanas " + precioManzanas);
+document.write(" es en total: " + precioTotal);
+document.write("<br/>");
+
+
+
+
+
+
+
+
+let precioManzana = 2.5;
+let precioPera = 1.5; 
+let precioPlatano = 1.8;
+let precioPerasmanzanas;
+
+precioPerasmanzanas = precioPera + precioManzana;
+
+document.write("Precio de peras = " + precioPera);
+document.write("Precio de manzanas = " + precioManzana);
+document.write("Precio de peras y manzanas = " + precioPerasmanzanas);
