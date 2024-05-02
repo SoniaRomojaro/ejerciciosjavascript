28b496956909e20ca944d28320a095ba1007502c
SoniaRomojaro <soniaromojaro@gmail.com>
2024-05-02 21:38:01 +0200

    cadenas

--- js/e4-dom-y-cadenas.js
+++ js/e4-dom-y-cadenas.js
@@ -1,5 +1,8 @@
 // JAVASCRIPR DOM. OBTENER ELEMENTOS POR SU ID
-//Obtener le parrafo
+//Obtener el parrafo
 let parrafoEjemplo = document.getElementById("ejemplo");
 //Modifcarf el texto del elemento
 parrafoEjemplo.innerText = "Hola Bootcamp QA"
+
+//Obterner el elemento html con id ejemploTitulo
+let tituloEjemplo = document.get
