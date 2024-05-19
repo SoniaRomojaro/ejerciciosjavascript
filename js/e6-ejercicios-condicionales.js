10679cf8abdd8568ecb5db8249e9f3b053174b8f
SoniaRomojaro <soniaromojaro@gmail.com>
2024-05-19 21:29:24 +0200

    condicionales

--- js/e6-ejercicios-condicionales.js
+++ js/e6-ejercicios-condicionales.js
@@ -13,3 +13,33 @@ function numeroMayordieciocho() {
     }
     imprimir(mensaje, "ejercicioEdad1");
 }
+
+// Ejercicio2 comparr números//
+function comparacionNumeros() {
+    let numeroCom1 = parseFloat(document.getElementById("ejerciocomparacion1").value);
+    let numeroCom2 = parseFloat(document.getElementById("ejerciocomparacion2").value);
+    let mensaje = "";
+
+    if (numeroCom1 >= numeroCom2) {
+        mensaje = "El numero 1 es mayor o igual que el número 2";
+
+    }
+    else {
+        mensaje = "El numero 2 es mayor que el número 1";
+    }
+    imprimir(mensaje, "ejercicioComparacionnumeros1");
+}
+function mayoriaEdad() {
+    let numeroCom1 = parseFloat(document.getElementById("ejerciomayoredad").value);
+    let mensaje = "";
+
+    if (numeroCom1 >= 18) {
+        mensaje = "Eres mayor de edad";
+
+    }
+    else {
+        mensaje = "Eres menor de edad";
+    }
+    imprimir(mensaje, "ejercicioMayoriadeedad");
+}
+
