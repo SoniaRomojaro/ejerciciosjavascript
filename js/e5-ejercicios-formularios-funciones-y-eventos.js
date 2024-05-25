d5cd0699ee0094cea163b998d27a27bc9e831ea0
SoniaRomojaro <soniaromojaro@gmail.com>
2024-05-25 17:26:40 +0200

    formularios y funciones

--- js/e5-ejercicios-formularios-funciones-y-eventos.js
+++ js/e5-ejercicios-formularios-funciones-y-eventos.js
@@ -1,4 +1,4 @@
-function wellcome(){
+function wellcome() {
     let nombre = document.getElementById("ej1Nombre").value;
     let ciudad = document.getElementById("ej1Ciudad").value;
     let fechaNacimiento = document.getElementById("ej1Fecha").value;
@@ -16,21 +16,21 @@ function calcularOperaciones() {
     let resultadoMultiplicar = numero1 * numero2;
     let resultadoDividir = numero1 / numero2;
 
- 
+
     let mensaje = "Los numeros " + numero1 + "y " + numero2 + "\n la suma: " + resultadoSumar + "\nResta: " + resultadoRestar + "\nMultiplicación: " + resultadoMultiplicar + "\nresultadoDividir: " + resultadoDividir;
-    
+
     imprimir(mensaje, "e2resultadOperaciones");
 }
 
-function cambioMoneda(){
+function cambioMoneda() {
     let num1euros = parseFloat(document.getElementById("e11number").value);
     let cambiolibras = 0.86;
     let cambiodolares = 1.08;
     let librastotales = num1euros * cambiolibras;
-    let dolarestotales = num1euros * cambiodolares; 
+    let dolarestotales = num1euros * cambiodolares;
     let mensaje = "La cantidad de euros " + num1euros + " equivale a  " + librastotales + " libras y a  " + dolarestotales + " dolares";
-  
-    imprimir(mensaje,"cambio");
+
+    imprimir(mensaje, "cambio");
 }
 
 /*
@@ -38,7 +38,7 @@ Crea un formulario para calcular precio total de un producto.
 El usuario debe introducir el nombre del producto, el precio y la cantidad. Agrega validación Boostrap. Todos los campos son obligatorios. El nombre debe tener entre 3 y 20 caracteres. El precio no puede ser negativo. La cantidad no puede ser negativa. 
 Agrega un boton que al hacer click llame a una función que muestre un mensaje con el nombre del producto, su cantidad, su precio y el precio total.
 */
-function precioTotal(){
+function precioTotal() {
     let nombreProducto = document.getElementById("nombreProducto").value;
     let precioProducto = document.getElementById("precioProducto").value;
     let cantidadProducto = document.getElementById("cantidadProducto").value;
@@ -46,7 +46,7 @@ function precioTotal(){
     let resultado = cantidadProducto * precioProducto;
     let mensaje = "Producto elegido:" + nombreProducto + " Precio: " + precioProducto + " cantidad: " + cantidadProducto + " tiene un precio total de " + resultado;
 
-   imprimir(mensaje,"precioTotal");
+    imprimir(mensaje, "precioTotal");
 }
 
 /*
@@ -59,9 +59,9 @@ Agrega un boton que al hacer click llame a una función que muestre un mensaje c
 function datosCamiseta() {
     let tallaCamiseta = document.getElementById("eje5TallaCamiseta").value;
     let colorCamiseta = document.getElementById("eje5ColorCamiseta").value;
-  
+
     let mensaje = "RESULTADO\nLa camiseta es de talla " + tallaCamiseta + " y de color " + colorCamiseta;
-    imprimir(mensaje,"eje5FormulariosResultado");
+    imprimir(mensaje, "eje5FormulariosResultado");
 
 }
 
@@ -71,55 +71,83 @@ La edad debe ser un numero positivo y es obligatorio. Agrega validación Boostra
 Agrega un boton que al hacer click llame a una función que muestre un mensaje con la edad en dias, la edad en minutos y la edad en segundos.
 
 */
-function calcularEdad(){
+function calcularEdad() {
     let edad = parseInt(document.getElementById("ej46Edad").value);
     const DIASANYO = 365;
     const HORASDIA = 24;
     const MINUTOSHORA = 60;
     const SEGUNDOSHORA = 60;
     let dias = edad * DIASANYO;
-    let min = dias * HORASDIA *  MINUTOSHORA;
+    let min = dias * HORASDIA * MINUTOSHORA;
     let sec = min * SEGUNDOSHORA;
     let mensaje = " Si tienes " + edad + " años" + " habrás vivido " + dias + " días o " + min + " minutos " + " o " + sec + " segundos. "
     imprimir(mensaje, "ex6resultado");
-  }
-
-
-  //Informacion texto Sonia//
-  function Informaciontexto(){
-    let Informaciondeltexto = (document.getElementById("ejer7texto").value);
-    let textoMayusculas = Informaciondeltexto.toUpperCase();
-    let primeraLetraejer7 = Informaciondeltexto[0];
-    let ultimaLetraejer7 = Informaciondeltexto[Informaciondeltexto.length - 1];
-    let contarejercicio7 = Informaciondeltexto.length;
-    let mensaje = "texto en mayúsculas: " +  textoMayusculas + "; Primera letra: " + primeraLetraejer7 + "; Ultima letra: " + ultimaLetraejer7 + "; contar letras: " + contarejercicio7;
-    imprimir(mensaje, "ex7resultado");
-  }
-  // Nota media//
-  function calcularNotamedia(){
-  let Nota1 = parseInt(document.getElementById("ej8Nota1").value);
-  let Nota2 = parseInt(document.getElementById("ej8Nota2").value);
-  let Nota3 = parseInt(document.getElementById("ej8Nota3").value);
-  let notaMedia = (Nota1 + Nota2 + Nota3)/ 3;
-  let mensaje = "Nota1: " + Nota1 + "; Nota 2: " + Nota2 + "; Nota 3: " + Nota3 + "; Nota media es igual a: " + notaMedia;
-  imprimir(mensaje, "ex8resultado");
 }
-// Ejercicio 9 stock//
-function Stock(){
-    let camisetas = parseInt(document.getElementById("ej9camisetas").value);
-    let zapatos = parseInt(document.getElementById("ej9zapatos").value);
-    let sombreros = parseInt(document.getElementById("ej9sombreros").value);
-    let camisetasNombre = "Camisetas"; 
-    let zapatosNombre = "Zapatos";
-    let sombrerosNombre = "Sombreros";
-    let mensaje = camisetasNombre + camisetas + zapatosNombre + zapatos + sombrerosNombre + sombreros;
-    imprimir(mensaje, "ejercicio9");
-   
+/*
+7. Crea un formulario para mostrar información sobre un texto.
+Crea un elemento textArea que reciba un texto de longitud entre 10 y 100 caracteres. Debe ser obligatorio. Agrega validación Boostrap.
+Agrega un boton que al hacer click llame a una función que muestre un mensaje con el texto completo en mayusculas, la primera letra del texto, la última letra y su longitud.
+*/
+
+function mostrarTexto() {
+    let texto = document.getElementById("e7texto").value;
+
+
+    let textoMayuscula = texto.toUpperCase();
+    let primeraLetra = texto.charAt(0); // texto[0]
+    let longitudTexto = texto.length;
+    let ultimaLetra = texto.charAt(longitudTexto - 1); //texto[longitudTexto-1]
+
+
+
+    let mensaje = " Texto en mayúscula: " + textoMayuscula + " \nLa primera letra: " + primeraLetra + " \nSu última letra: " + ultimaLetra + " \nunto con su longitud: " + longitudTexto;
+    imprimir(mensaje, "e7resultadoTexto");
 }
 
+/*
+8. Crea un formulario para calcular la nota media.
+Agrega 3 elementos que reciban numeros entre 0 y 10 y que no pueden estar vacío. Agrega validación Boostrap.
+Agrega un boton que al hacer click llame a una función que muestre un mensaje con la media de los tres números.
+ */
+// Nota media//
+function calcularNotamedia() {
+    let Nota1 = parseInt(document.getElementById("ej8Nota1").value);
+    let Nota2 = parseInt(document.getElementById("ej8Nota2").value);
+    let Nota3 = parseInt(document.getElementById("ej8Nota3").value);
+    let notaMedia = (Nota1 + Nota2 + Nota3) / 3;
+    let mensaje = "Nota1: " + Nota1 + "; Nota 2: " + Nota2 + "; Nota 3: " + Nota3 + "; Nota media es igual a: " + notaMedia;
+    imprimir(mensaje, "ex8resultado");
+}
+/**
+ * 9. Crea un formulario para que el usuario pueda seleccionar una cantidad de camisetas, que puede ser vacio, 1,2 o 3, una cantidad de zapatos también entre vacio, 1,2 o 3 y una cantidad de sombreros, que puede ser vacio, 1,2, o 3.
+Agrega 1 elemento para seleccionar la cantidad de cada producto. El elemento select puede estar vacio.
+Agrega un boton que al hacer click llame a una función que muestre un mensaje con el nombre de cada producto y su cantidad. Si ha dejado vacio, no mostrará nada en su cantidad. */
 
-    
-    
+function mostrarProducto() {
+    let numCamiseta = document.getElementById("ej9camiseta").value;
+    let numZapatos = document.getElementById("ej5zapatos").value;
+    let numSombreros = document.getElementById("ej5sombreros").value;
+
+    let camiseta = "Camiseta";
+    let zapatos = "Zapatos";
+    let sombreros = "Sombreros";
+
+    let mensaje = "El producto " + camiseta + " hay " + numCamiseta + " unidades, el producto " + zapatos + " hay " + numZapatos + " unidades y el producto "
+        + sombreros + " hay " + numSombreros + " unidades. "
 
 
+    imprimir(mensaje, "respuesta9");
+}
 
+/**
+ * 10. Crea un formulario para que el usuario pueda introducir el nombre de su mascota, y seleccionar el tipo: perro, gato, tortuga, pajaro.
+Agrega 1 elemento agregar el nombre de la mascota y el tipo. Ambos pueden estar vacios (el usuario puede no tener mascota, o no ser de ninguno de esos tipos)
+Agrega un boton que al hacer click llame a una función que muestre un mensaje con los datos de la mascota, es decir, nombre y tipo.  */
+function nombreTipo() {
+    let nombre = document.getElementById("nombremascota").value;
+    let mascota = document.getElementById("mascota").value;
+
+
+    let mensaje = " Hola te llamas " + nombre + " y eres un/una !" + mascota;
+    imprimir(mensaje, "eje10Resultado");
+}
